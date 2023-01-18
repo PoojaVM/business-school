abda012780f459b8210a594fcbf12b9687499a70
Pooja Mule <poojamule@Poojas-MacBook-Pro.local>
2023-01-18 12:53:22 -0500

    Business school events

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { EVENTS, MONTH_NAMES } from './EventsData';
+import { SocialIcon } from 'react-social-icons'
 
 function App() {
+  const rows = EVENTS.map((item, index) => 
+  <div className='Event-Container'>
+    <div className='Each-Event'>
+      <span style={{fontSize: '1rem', lineHeight: 1.125, fontFamily: 'Saira Extra Condensed,sans-serif', fontStyle: 'normal', fontWeight: 700}}>{MONTH_NAMES[item.month]} {item.date}, {item.year}</span>
+      <span style={{color: '#a32638', fontSize: '1.575rem', fontFamily: 'Saira Extra Condensed,sans-serif', fontStyle: 'normal', fontWeight: 400}}>{item.title}</span>
+      <span style={{fontFamily: 'Bitter,serif', fontStyle: 'normal', fontWeight: 400, fontSize: '1.1rem', color: '#000', marginTop: '0.625rem'}}>{item.description}</span>
+      <span style={{fontFamily: 'Saira Extra Condensed,sans-serif', fontStyle: 'normal', fontSize: '0.9rem', fontWeight: 400, marginTop: '0.425rem', color: '#363d45', lineHeight: 1.125}}>{item.location}</span>
+      <span style={{fontFamily: 'Saira Extra Condensed,sans-serif', fontStyle: 'normal', fontSize: '0.9rem', fontWeight: 400, color: '#363d45', lineHeight: 1.125}}>{item.time}</span>
+    </div>
+    <div className='Image'>
+      <span>Image goes here</span>
+    </div>
+  </div>
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='App-header'>
+        STEVENS SCHOOL OF BUSINESS EVENTS
       </header>
+      <tbody className='List'>{rows}</tbody>
+      <footer className='App-Footer'>
+        <SocialIcon network="facebook" className='Icon' url="https://www.facebook.com/hanlonlabs" target="_blank" />
+        <SocialIcon network="twitter" className='Icon' url="https://twitter.com/hanlonlab" target="_blank" />
+        <SocialIcon network="instagram" className='Icon' url="https://www.instagram.com/hanlonlab" target="_blank" />
+        <SocialIcon network="email" className='Icon' url="mailto:fscadmin@stevens.edu" />
+      </footer>
     </div>
   );
 }
 
 export default App;
+
+
+        // {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        // {/* <p>
+        //   Edit <code>src/App.js</code> and save to reload.
+        // </p> */}
+        // {/* <a
+        //   className="App-link"
+        //   href="https://reactjs.org"
+        //   target="_blank"
+        //   rel="noopener noreferrer"
+        // >
+        //   Learn React
+        // </a> */}
---
+++ src/EventsData.js
@@ -0,0 +1,201 @@
+// Event name, location, date, time
+export const EVENTS = [
+  {
+    title: 'Naples Network Luncheon',
+    type: 'public',
+    time: '12:00PM to 2:00PM',
+    month: 1,
+    year: 2023,
+    date: 20,
+    location: 'Stevens Institute of Technology',
+    requireRegistration: true,
+    address: {
+      apartment: 'Hyatt Regency Coconut Point',
+      lane: '5001 Coconut Rd',
+      city: 'Bonita Springs',
+      state: 'FL',
+      zipCode: 34134
+    },
+    description: 'Join your local Naples Network, led by Frank Semcer ’65, to reconnect and hear the latest news from Stevens.'
+  },
+  {
+    title: 'State of Stevens in Florida',
+    type: 'public',
+    time: '12:00PM to 2:00PM',
+    month: 1,
+    year: 2023,
+    date: 20,
+    location: 'Off Campus',
+    requireRegistration: true,
+    address: {
+      apartment: 'Hyatt Regency Coconut Point',
+      lane: '5001 Coconut Rd',
+      city: 'Bonita Springs',
+      state: 'FL',
+      zipCode: 34134
+    },
+    description: 'Please join President Nariman Farvardin as he returns to Florida to provide the very latest on the State of Stevens.'
+  },
+  {
+    title: 'Industry Torchbearers in Information Technology',
+    type: 'public',
+    time: '12:00PM to 2:00PM',
+    month: 1,
+    year: 2023,
+    date: 20,
+    location: 'Virtual',
+    requireRegistration: true,
+    address: {
+      apartment: 'Hyatt Regency Coconut Point',
+      lane: '5001 Coconut Rd',
+      city: 'Bonita Springs',
+      state: 'FL',
+      zipCode: 34134
+    },
+    description: 'Join the IT Industry Torchbearers and Professor Yuping Huang for their January presentation.'
+  },
+  {
+    title: 'Julia Boorstin - When Women Lead',
+    type: 'public',
+    time: '12:00PM to 2:00PM',
+    month: 1,
+    year: 2023,
+    date: 20,
+    location: 'Off Campus',
+    requireRegistration: true,
+    address: {
+      apartment: 'Hyatt Regency Coconut Point',
+      lane: '5001 Coconut Rd',
+      city: 'Bonita Springs',
+      state: 'FL',
+      zipCode: 34134
+    },
+    description: 'Julia Boorstin, CNBC’s Senior Media & Tech Correspondent will discuss her book When Women Lead.'
+  },
+  {
+    title: `Campus Point Connection: 'Eddie Allen Aggregation' Big Band`,
+    type: 'public',
+    time: '12:00PM to 2:00PM',
+    month: 1,
+    year: 2023,
+    date: 20,
+    location: 'Off Campus',
+    requireRegistration: true,
+    address: {
+      apartment: 'Hyatt Regency Coconut Point',
+      lane: '5001 Coconut Rd',
+      city: 'Bonita Springs',
+      state: 'FL',
+      zipCode: 34134
+    },
+    description: 'The College of Arts and Letters welcomes the Eddie Allen Aggregation, a big band of jazz musicians.'
+  },
+  {
+    title: `ECG Distinguished Lecture Series: Understanding Technical and Economic Limits to Power Grid Decarbonization`,
+    type: 'public',
+    time: '12:00PM to 2:00PM',
+    month: 1,
+    year: 2023,
+    date: 20,
+    location: 'Virtual',
+    requireRegistration: true,
+    address: {
+      apartment: 'Hyatt Regency Coconut Point',
+      lane: '5001 Coconut Rd',
+      city: 'Bonita Springs',
+      state: 'FL',
+      zipCode: 34134
+    },
+    description: 'Department of Electrical and Computer Engineering'
+  },
+  {
+    title: `Dual Plasmepsin IX/X Inhibitors: An Antimalarial Drug Discovery Collaboration`,
+    type: 'public',
+    time: '12:00PM to 2:00PM',
+    month: 1,
+    year: 2023,
+    date: 20,
+    location: 'Off Campus',
+    requireRegistration: true,
+    address: {
+      apartment: 'Hyatt Regency Coconut Point',
+      lane: '5001 Coconut Rd',
+      city: 'Bonita Springs',
+      state: 'FL',
+      zipCode: 34134
+    },
+    description: 'Department of Chemistry and Chemical Biology'
+  },
+  {
+    title: `Stevens Day of Giving`,
+    type: 'public',
+    time: '12:00PM to 2:00PM',
+    month: 1,
+    year: 2023,
+    date: 20,
+    location: 'Stevens Institute of Technology',
+    requireRegistration: true,
+    address: {
+      apartment: 'Hyatt Regency Coconut Point',
+      lane: '5001 Coconut Rd',
+      city: 'Bonita Springs',
+      state: 'FL',
+      zipCode: 34134
+    },
+    description: 'In honor of our founding, the Stevens community will come together at Castle Point for 1,870 minutes to support the causes that mean the most to them.'
+  },
+  {
+    title: `151st Commencement Ceremonies`,
+    type: 'public',
+    time: '12:00PM to 2:00PM',
+    month: 1,
+    year: 2023,
+    date: 20,
+    location: 'Off Campus',
+    requireRegistration: true,
+    address: {
+      apartment: 'Hyatt Regency Coconut Point',
+      lane: '5001 Coconut Rd',
+      city: 'Bonita Springs',
+      state: 'FL',
+      zipCode: 34134
+    },
+    description: 'Stevens Institute of Technology will hold its 151st Commencement Ceremonies at the Prudential Center in Newark, New Jersey on Wednesday, May 24, 2023.'
+  },
+  {
+    title: `Alumni Weekend`,
+    type: 'public',
+    time: '12:00PM to 2:00PM',
+    month: 1,
+    year: 2023,
+    date: 20,
+    location: 'Stevens Institute of Technology',
+    requireRegistration: true,
+    address: {
+      apartment: 'Hyatt Regency Coconut Point',
+      lane: '5001 Coconut Rd',
+      city: 'Bonita Springs',
+      state: 'FL',
+      zipCode: 34134
+    }
+  }
+];
+
+export const MONTH_NAMES = {
+  1: 'January',
+  2: 'February',
+  3: 'March',
+  4: 'April',
+  5: 'May',
+  6: 'June',
+  7: 'July',
+  8: 'August',
+  9: 'September',
+  10: 'October',
+  11: 'November',
+  12: 'December'
+};
+
+export const LOCATIONS = [
+  'On Campus', 'Off Campus', 'Virtual'
+];
